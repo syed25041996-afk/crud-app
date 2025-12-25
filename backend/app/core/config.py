ef68880fecd39abebd38b8b522056484c9695e46
syed25041996 <syed25041996@gmail.com>
2025-12-25 17:28:56 +0530

    Configured server database and added to the backend

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,12 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-from pathlib import Path
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+load_dotenv()
 
 class Settings:
-    PROJECT_NAME:str = "Job Board"
+    PROJECT_NAME:str = "Products app"
     PROJECT_VERSION: str = "1.0.0"
 
     POSTGRES_USER : str = os.getenv("POSTGRES_USER")
@@ -14,6 +12,6 @@ class Settings:
     POSTGRES_SERVER : str = os.getenv("POSTGRES_SERVER","localhost")
     POSTGRES_PORT : str = os.getenv("POSTGRES_PORT",5432) # default postgres port is 5432
     POSTGRES_DB : str = os.getenv("POSTGRES_DB","tdd")
-    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
+    DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}/{POSTGRES_DB}"
 
 settings = Settings()
--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from core.config import settings
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@localhost/product_db'
+# print("=================================================")
+# print(settings.DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
+# SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:1234@192.168.1.103/product_db'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
