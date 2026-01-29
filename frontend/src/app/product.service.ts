27225a3ad39a58c8e8d974bc2c4952e7249d9a35
syed25041996-afk <syed25041996@gmail.com>
2026-01-29 11:33:05 +0530

    modify service file in product

--- frontend/src/app/product.service.ts
+++ frontend/src/app/product.service.ts
@@ -8,6 +8,7 @@ import { Product } from './product';
 })
 export class ProductService {
   // private apiUrl = 'http://home.tail2b1f38.ts.net:8000/api/products';
+  // For tailscale service testing purpose, use the below URL
   private apiUrl = 'http://default-backend.tail2b1f38.ts.net:8000/api/products';
 
   constructor(private http: HttpClient) { }
